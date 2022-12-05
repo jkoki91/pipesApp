2cf766127cc91242b37cdb9821d1904891c530bd
Jeff Worker <>
2022-12-05 15:58:36 +0100

    Components creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// Modulo personalizado
+import { SharedModule } from './shared/shared.module';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ventas/ventas.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NumerosComponent } from './pages/numeros/numeros.component';
+import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
+import { BasicosComponent } from './pages/basicos/basicos.component';
+
+
+
+@NgModule({
+  declarations: [
+    NumerosComponent,
+    NoComunesComponent,
+    BasicosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class VentasModule { }
